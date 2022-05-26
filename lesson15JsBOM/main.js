e670cd5920f67b755a7ea6777596eb75925e8f3a
AlbertVoieikov <63648832+AlbertVoieikov@users.noreply.github.com>
2022-05-26 15:35:33 +0300

    add lesson 15 homework JS BOM

---
+++ lesson15JsBOM/main.js
@@ -0,0 +1,67 @@
+// =================== № 1 ====================
+
+// -створити форму з інпутами для name та age.
+// При відправці форми записати об'єкт в localstorage
+
+// let userForm = document.forms.userform;
+//
+// let btnSub = document.getElementById('submit')
+// btnSub.onclick = function () {
+//
+//     let userNameInput = userform.username;
+//     let userAgeInput = userform.userage;
+//
+//     let userName = userNameInput.value;
+//     let userAge = userAgeInput.value;
+//
+//     let user = {
+//         username: userName,
+//         userage: userAge,
+//     }
+//     // console.log(user);
+//     // userNameInput.value = '';
+//     // userAgeInput.value = '';
+//
+//
+//     localStorage.setItem('user', JSON.stringify(user));
+//     let userJSON = localStorage.getItem('user');
+//     console.log(userJSON);
+//     let userParse = JSON.parse(userJSON);
+//     console.log(userParse);
+// }
+
+// ____________________________________________
+
+// =================== № 2 ====================
+
+// -створити форму з інпутами для model,type та volume автівки.
+// при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
+
+// let carForm = document.forms.carform;
+//
+// let carBtnSub = document.getElementById('carsubmit')
+// carBtnSub.onclick = function () {
+//
+//     let carModelInput = carForm.carmodel;
+//     let carTypeInput = carForm.cartype;
+//     let carVolumeInput = carForm.carvolume;
+//
+//     let carModel = carModelInput.value;
+//     let carType = carTypeInput.value;
+//     let carVolume = carVolumeInput.value;
+//
+//     let car = [
+//         carmodel = carModel,
+//         cartype = carType,
+//         carvolume = carVolume
+//     ]
+//     console.log(car);
+//
+//     localStorage.setItem('car', JSON.stringify(car));
+//     let carJSON = localStorage.getItem('car');
+//     console.log(carJSON);
+//     let carParse = JSON.parse(carJSON);
+//     console.log(carParse);
+// }
+
+// ____________________________________________
