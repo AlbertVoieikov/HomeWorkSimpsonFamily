c9b6a5ec739b24e7be7451886f84e82d8679de65
AlbertVoieikov <63648832+AlbertVoieikov@users.noreply.github.com>
2022-06-06 15:37:59 +0300

    add final JS project

--- finalJsProject/main.js
+++ finalJsProject/main.js
@@ -4,73 +4,6 @@
 // 3 Добавить каждому блоку кнопку/ссылку , при клике на которую происходит переход на страницу user-details.html,
 // которая имеет детальную информацию про объект на который кликнули
 
-fetch(`https://jsonplaceholder.typicode.com/users`)
-    .then(value => value.json())
-    .then(value => {
-        const wrapDiv = document.createElement(`div`);
-
-        document.body.append(wrapDiv);
-        for (const user of value) {
-            const divContainer = document.createElement(`div`);
-            wrapDiv.append(divContainer);
-            const {id, name} = user;
-            divContainer.innerHTML = `
-                <p>UserId: ${user.id}</p>
-                <p>Name: ${user.name}</p>`
-            divContainer.style.border = `1px solid blue`;
-
-            const userDetailsBtn = document.createElement(`button`);
-            userDetailsBtn.id = `userBtn`;
-
-            userDetailsBtn.innerHTML = `<a href="user-details.html?id=${user.id}">User details</a>`
-            divContainer.append(userDetailsBtn);
-
-            userDetailsBtn.onclick = () => {
-                fetch(`https://jsonplaceholder.typicode.com/users${user.id}`)
-                    .then(value => value.json())
-                    .then(value => {
-                        const userContainer = document.createElement(`div`);
-                        divContainer.append(userContainer);
-                        for (const userContainerElement of value) {
-                            const userDiv = document.createElement(`div`);
-                            userDiv.innerHTML = `
-                                <p>id: ${user.id}</p>
-                                <p>name: ${user.name} </p>
-                                <p>username: ${user.username}</p>
-                                <p>email: ${user.email}</p>
-                                <p>address:
-                                     <p>street: ${user.address.street}</p>
-                                     <p>suite: ${user.address.suite}</p>
-                                     <p>city: ${user.address.city}</p>
-                                     <p>zipcode: ${user.address.zipcode}</p>
-                                     <p>geo:
-                                         <p>- lat: ${user.address.geo.lat}</p>
-                                         <p>- lng: ${user.address.geo.lng}</p>
-                                     </p>
-                                </p>
-                                <p>phone: ${user.phone}</p>
-                                <p>website: ${user.website}</p>
-                                <p>company:
-                                    <p>name: ${user.company.name}</p>
-                                    <p>catchPhrase: ${user.company.catchPhrase}</p>
-                                    <p>bs: ${user.company.bs}</p>
-                                </p>
-                                `
-
-                            userContainer.append(userDiv);
-                            // userDetailsBtn.disabled = true;
-
-
-                        }
-                    })
-            }
-        }
-
-    });
-
-
-
-
 // На странице user-details.html:
 // 4 Вывести всю, без исключения, информацию про объект user на кнопку/ссылку которого был совершен клик ранее.
 // 5 Добавить кнопку "post of current user", при клике на которую, появляются title всех постов текущего юзера
@@ -92,3 +25,28 @@ fetch(`https://jsonplaceholder.typicode.com/users`)
 // post-details.html - блок с информацией про пост вверху. Комментарии - по 4 в ряд.
 // Все без исключения элементы, который характеризируют user,post,comment  визуализировать,
 // так, что бы было видно их блоки (дать задний фон + margin. Иными словами - крайне четкая сетка)
+
+
+fetch(`https://jsonplaceholder.typicode.com/users`)
+.then(value => value.json())
+.then(users => {
+    for (const user of users) {
+        const userDiv = document.createElement(`div`);
+        userDiv.classList = `user`;
+        const h2Element = document.createElement(`h2`);
+        h2Element.innerText = user.id + `. ` + user.name;
+        const formElement = document.createElement(`form`);
+        formElement.setAttribute(`action`, `user-details.html`);
+        const buttonElement = document.createElement(`button`);
+        buttonElement.innerText = `User details`;
+        buttonElement.classList = `button`;
+        formElement.append(buttonElement);
+        userDiv.append(h2Element, formElement);
+        document.body.append(userDiv);
+        buttonElement.onclick = () =>{
+            localStorage.setItem(`user`, JSON.stringify(user));
+        }
+
+    }
+})
+
--- finalJsProject/user-details.js
+++ finalJsProject/user-details.js
@@ -1,179 +1,39 @@
-const userDetails = document.getElementById(`userDet`);
-const userUrl = new URL(`https://jsonplaceholder.typicode.com/users`)
-const userId = new URLSearchParams(location.search).get('id');
+let userDetails = localStorage.getItem(`user`);
+console.log(userDetails);
 
-userId && fetch(`https://jsonplaceholder.typicode.com/users`)
+const wrapUserDetails = document.getElementById(`userDet`);
+fetch(`https://jsonplaceholder.typicode.com/users`)
     .then(value => value.json())
     .then(value => {
-        const userDetails = document.getElementById(`userDet`);
-        for (const user of value) {
-            const userDetailsDiv = document.createElement(`div`);
-            userDetails.appendChild(userDetailsDiv);
-            for (const userKey in user) {
+        for (const userItem of value) {
                 const divElement = document.createElement(`div`);
+                wrapUserDetails.append(divElement);
                 // divElement.innerText = `${userKey}: ${user[userKey]}`;
                 divElement.innerHTML = `
-                <p>id: ${user.id}</p>
-                <p>name: ${user.name} </p>
-                <p>username: ${user.username}</p>
-                <p>email: ${user.email}</p>
+                <p>id: ${userItem.id}</p>
+                <p>name: ${userItem.name} </p>
+                <p>username: ${userItem.username}</p>
+                <p>email: ${userItem.email}</p>
                 <p>address:
-                     <p>street: ${user.address.street}</p>
-                     <p>suite: ${user.address.suite}</p>
-                     <p>city: ${user.address.city}</p>
-                     <p>zipcode: ${user.address.zipcode}</p>
+                     <p>street: ${userItem.address.street}</p>
+                     <p>suite: ${userItem.address.suite}</p>
+                     <p>city: ${userItem.address.city}</p>
+                     <p>zipcode: ${userItem.address.zipcode}</p>
                      <p>geo:
-                         <p>- lat: ${user.address.geo.lat}</p>
-                         <p>- lng: ${user.address.geo.lng}</p>
+                         <p>- lat: ${userItem.address.geo.lat}</p>
+                         <p>- lng: ${userItem.address.geo.lng}</p>
                      </p>
                 </p>
-                <p>phone: ${user.phone}</p>
-                <p>website: ${user.website}</p>
+                <p>phone: ${userItem.phone}</p>
+                <p>website: ${userItem.website}</p>
                 <p>company:
-                    <p>name: ${user.company.name}</p>
-                    <p>catchPhrase: ${user.company.catchPhrase}</p>
-                    <p>bs: ${user.company.bs}</p>
+                    <p>name: ${userItem.company.name}</p>
+                    <p>catchPhrase: ${userItem.company.catchPhrase}</p>
+                    <p>bs: ${userItem.company.bs}</p>
                 </p>`
-                userDetails.appendChild(divElement);
-                document.getElementById(`userBtn`)
-                // userDetailsBtn.disabled = true;
-            }
+
+            // const buttonElement = document.getElementsByClassName(`button`)
+            // buttonElement.disabled = true;
+
         }
-
     })
-
-
-
-
-
-
-
-
-
-
-// <p>id: ${user.id}</p>
-// <p>name: ${user.name} </p>
-// <p>username: ${user.username}</p>
-// <p>email: ${user.email}</p>
-// <p>address:
-//      <p>street: ${user.address.street}</p>
-//      <p>suite: ${user.address.suite}</p>
-//      <p>city: ${user.address.city}</p>
-//      <p>zipcode: ${user.address.zipcode}</p>
-//      <p>geo:
-//          <p>- lat: ${user.address.geo.lat}</p>
-//          <p>- lng: ${user.address.geo.lng}</p>
-//      </p>
-// </p>
-// <p>phone: ${user.phone}</p>
-// <p>website: ${user.website}</p>
-// <p>company:
-//     <p>name: ${user.company.name}</p>
-//     <p>catchPhrase: ${user.company.catchPhrase}</p>
-//     <p>bs: ${user.company.bs}</p>
-// </p>
-
-
-
-// let usersContainer = document.getElementById(`users`)
-// usersContainer && fetch(`https://jsonplaceholder.typicode.com/users`)
-//     .then(response => response.json())
-//     .then(response => {
-//         for (const user of response) {
-//             const userDiv = document.createElement(`div`);
-//             userDiv.id = `userDiv`;
-//             userDiv.classList = `userDiv`;
-//             userDiv.innerHTML = `
-//                 <p>user id - ${user.id}</p>
-//                 <p>user name - ${user.name}</p>`;
-//
-//             usersContainer.appendChild(userDiv);
-//
-//             const button = document.createElement(`button`);
-//             button.id = `userButton`;
-//             button.innerHTML = `<a href="user-details.html?id=${user.id}">User details</a>`;
-//             userDiv.appendChild(button);
-//         }
-//     })
-//
-// const userDetails = document.getElementById(`userDet`);
-// const userUrl = new URL(`https://jsonplaceholder.typicode.com/users`)
-// const userId = new URLSearchParams(location.search).get('id');
-//
-// userId && fetch(`https://jsonplaceholder.typicode.com/users`)
-//     .then(value => value.json())
-//     .then(value => {
-//         const userDetails = document.getElementById(`userDet`);
-//         for (const user of value) {
-//             const userDetailsDiv = document.createElement(`div`);
-//             userDetails.appendChild(userDetailsDiv);
-//             for (const userKey in user) {
-//                 const divElement = document.createElement(`div`);
-//                 divElement.innerText = `${userKey}: ${user[userKey]}`;
-//                 userDetails.appendChild(divElement);
-//             }
-//         }
-//
-//     })
-
-
-
-
-//_______________________________________________________________
-
-// fetch(`https://jsonplaceholder.typicode.com/users`)
-//     .then(value => value.json())
-//     .then(value => {
-//         const wrapDiv = document.createElement(`div`);
-//         document.body.append(wrapDiv);
-//         for (const user of value) {
-//             const divContainer = document.createElement(`div`);
-//             wrapDiv.append(divContainer);
-//             const {id, name} = user;
-//             divContainer.innerHTML = `
-//             <p>UserId: ${user.id}</p>
-//             <p>Name: ${user.name}</p>`
-//
-//             const userDetailsBtn = document.createElement(`button`);
-//
-//             userDetailsBtn.innerHTML = `<a href="https://jsonplaceholder.typicode.com/users/${user.id}">User details</a>`
-//             divContainer.append(userDetailsBtn);
-//
-//             userDetailsBtn.onclick = () => {
-//                 fetch(`https://jsonplaceholder.typicode.com/users`)
-//                     .then(value => value.json())
-//                     .then(value => {
-//                         const userContainer = document.createElement(`div`);
-//                         divContainer.append(userContainer);
-//                         for (const userContainerElement of value) {
-//                             const userDiv = document.createElement(`div`);
-//                             userDiv.innerHTML = `
-//                                 <p>id: ${user.id}</p>
-//                                 <p>name: ${user.name} </p>
-//                                 <p>username: ${user.username}</p>
-//                                 <p>email: ${user.email}</p>
-//                                 <p>address:
-//                                      <p>street: ${user.address.street}</p>
-//                                      <p>suite: ${user.address.suite}</p>
-//                                      <p>city: ${user.address.city}</p>
-//                                      <p>zipcode: ${user.address.zipcode}</p>
-//                                      <p>geo:
-//                                          <p>- lat: ${user.address.geo.lat}</p>
-//                                          <p>- lng: ${user.address.geo.lng}</p>
-//                                      </p>
-//                                 </p>
-//                                 <p>phone: ${user.phone}</p>
-//                                 <p>website: ${user.website}</p>
-//                                 <p>company:
-//                                     <p>name: ${user.company.name}</p>
-//                                     <p>catchPhrase: ${user.company.catchPhrase}</p>
-//                                     <p>bs: ${user.company.bs}</p>
-//                                 </p>
-//                                 `
-//                             userContainer.append(userDiv);
-//                         }
-//                     })
-//             }
-//         }
-//
-//     });
